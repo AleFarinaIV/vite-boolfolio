469d3d9ee364dc7bc1948fdfd536a46b48935b27
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-15 19:52:21 +0200

    [UPD] corrected api call and passed data into v-for

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ export default {
     getAllProjects() {
       axios.get('http://127.0.0.1:8000/api/projects')
       .then((response) => {
-          this.projects = response.data;
+          this.projects = response.data.results;
+          console.log(response.data.results);
       });
     }
   },
@@ -23,16 +24,16 @@ export default {
 <template lang="">
     <div class="container">
       <div class="row">
-        <div class="col-12">
+        <div class="col-12 p-5">
           <h1 class="text-center">Elenco progetti</h1>
         </div>
         <div class="col-12">
-          <div class="row">
-            <div class="col-12 col-md-6 col-lg-4">
-              <div class="card">
-                <img class="card-img-top" src="">
+          <div class="row gy-4">
+            <div class="col-12 col-md-6 col-lg-4" v-for="proj in projects" :key="proj.id">
+              <div class="card min-h">
+                <img class="card-img-top" :src="proj.image_path != null ? `http://127.0.0.1:8000/storage/${proj.image_path}` : 'https://placehold.co/600x400?text=No+path+image'">
                 <div class="card-body">
-                  <h4 class="card-title">Title</h4>
+                  <h4 class="card-title">{{ proj.name }}</h4>
                   <span>Lorem ipsum</span>
                 </div>
               </div>
@@ -44,5 +45,9 @@ export default {
 </template>
 
 <style lang="scss">
-@import './styles/generals.scss'
+@import './styles/generals.scss';
+
+  .min-h {
+    min-height: 290px;
+  }
 </style>
