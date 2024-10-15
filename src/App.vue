b2f08e8d0f355a2afd4ef2489a9824feec27e0f2
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-15 18:49:48 +0200

    [UPD] first vue application setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods:{
+    getAllProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+      .then((response) => {
+          // this.projects = response.data;
+          console.log(response.data);
+      });
+    }
+  },
+  created() {
+    this.getAllProjects();
+  }
+}
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center">Elenco progetti</h1>
+        </div>
+        <div class="col-12">
+
+        </div>
+      </div>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@import './styles/generals.scss'
 </style>
