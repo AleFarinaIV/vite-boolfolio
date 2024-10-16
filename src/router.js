2477d5d87aa5a8e1cbcdceb0538787ddf840769b
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-16 15:51:56 +0200

    import and export component and router into router.js

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory} from 'vue-router';
+import PageHome from './pages/PageHome.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path:'/',
-            name: 'home',
-            // component: 
+            name: 'homepage',
+            component: PageHome,
         }
     ]
 })
+
+export { router }
