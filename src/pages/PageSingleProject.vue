19c688f1a5d976af2fa8008c698023bb2b470ca4
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-16 21:38:06 +0200

    [UPD] created new component for single project

---
+++ src/pages/PageSingleProject.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js';
+export default {
+    data() {
+        return {
+            store,
+            project: null
+        }
+    },
+    methods: {
+        getSingleProject() {
+            console.log(this.$route)
+            axios.get(`${ apiUrl }/projects/${ this.$route.params.slug }`)
+            .then((response) => {
+                console.log(response);
+            })
+        }
+    },
+    created() {
+        this.getSingleProject();
+    }
+}
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
