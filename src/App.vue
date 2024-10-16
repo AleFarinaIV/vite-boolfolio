5763ec4b621b7680a3abcdb410442228645f6236
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-16 18:54:26 +0200

    [UPD] created and setup header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import ProjectCard from './components/ProjectCard.vue';
+import AppHeader from './components/AppHeader.vue';
 export default {
   data() {
     return {
@@ -10,7 +11,8 @@ export default {
     }
   },
   components: {
-    ProjectCard
+    ProjectCard,
+    AppHeader,
   },
   methods:{
     getAllProjects() {
@@ -38,35 +40,45 @@ export default {
 
 <template>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-12 p-5">
-        <h1 class="text-center">Elenco progetti</h1>
-      </div>
-      <div class="col-12">
-        <div class="row gy-4">
-          <ProjectCard v-for="proj in projects"
-          :key="proj.id"
-          :project="proj"/>
+  <AppHeader/>
+
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 p-5">
+          <h1 class="text-center">Elenco progetti</h1>
         </div>
-      </div>
-      <div class="col-12 d-flex justify-content-center p-4">
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item" :class="current_page == 1 ? 'disabled' : ''"><a class="page-link" href="#" @click="goToPage(current_page - 1)">Previous</a></li>
-              <li class="page-item" v-for="i in last_page"><a class="page-link" href="#" @click="goToPage(i)">{{ i }}</a></li>
-              <li class="page-item" :class="current_page == last_page ? 'disabled' : ''"><a class="page-link" href="#" @click="goToPage(current_page + 1)">Next</a></li>
-            </ul>
-          </nav>
+        <div class="col-12">
+          <div class="row gy-4">
+            <ProjectCard v-for="proj in projects"
+            :key="proj.id"
+            :project="proj"/>
+          </div>
         </div>
+        <div class="col-12 d-flex justify-content-center p-4">
+            <nav aria-label="Page navigation example">
+              <ul class="pagination">
+                <li class="page-item" :class="current_page == 1 ? 'disabled' : ''"><a class="page-link" href="#" @click="goToPage(current_page - 1)">Previous</a></li>
+                <li class="page-item" v-for="i in last_page"><a class="page-link" href="#" @click="goToPage(i)">{{ i }}</a></li>
+                <li class="page-item" :class="current_page == last_page ? 'disabled' : ''"><a class="page-link" href="#" @click="goToPage(current_page + 1)">Next</a></li>
+              </ul>
+            </nav>
+          </div>
+      </div>
     </div>
-  </div>
+  </main>
+
 </template>
 
 <style lang="scss">
 @import './styles/generals.scss';
 
-  .min-h {
-    min-height: 290px;
+  main {
+    height: calc(100vh - 80px);
+    margin-top: 80px;
+
+    .min-h {
+      min-height: 290px;
+    }
   }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <div>
+                        <h4 class="text-white">Qui andrà un logo</h4>
+                        <!-- <img src="" alt="logo"> -->
+                    </div>
+                    <ul class="list-unstyled d-flex m-0">
+                        <li class="me-3">
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Projects</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+</template>
+
+<style lang="scss" scoped>
+    header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1000;
+        background-color: black;
+        width: 100%;
+        height: 80px;
+        display: flex;
+        align-items: center;
+
+        a {
+            text-decoration: none;
+            color: white;
+            font-size: 18px;
+            &:hover {
+                color: grey;
+            }
+        }
+    }
+</style>
